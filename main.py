e9a011c0a955f9f05228de3c492914c9f7b9e890
stkraine <stkraine@gmail.com>
2021-10-09 22:35:51 -0400

    moved files around

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ RECORDING = False
 while(True):
     event, values = window.read(timeout=1)
 
-
+    # checks if the start recording button has been pressed
     if "__RECORD__" == event:
         RECORDING = flip_botton_color("__RECORD__", RECORDING)
 
-    
+    # checks if the window is to be closed
     if event in (sg.WIN_CLOSED, 'Cancel'):
         break
         
